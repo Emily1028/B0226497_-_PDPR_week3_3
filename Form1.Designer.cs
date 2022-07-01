a3074b1e802350d6b792b79874cb9c0feac98c68
Emily1028 <81636593+Emily1028@users.noreply.github.com>
2022-07-01 23:54:46 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace B0226497_段昱如_PDPR_week3_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblresult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(89, 107);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(112, 35);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "確定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(317, 107);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 35);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "結束";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "B0226497_段昱如_PDPR_week3_3";
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Location = new System.Drawing.Point(85, 189);
+            this.lblresult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(83, 24);
+            this.lblresult.TabIndex = 3;
+            this.lblresult.Text = "lblresult";
+            this.lblresult.Click += new System.EventHandler(this.lblresult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 500);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOK);
+            this.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblresult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B0226497_段昱如_PDPR_week3_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+            student stu_1 = new student();
+            stu_1.setSID("B0226497_1");
+            stu_1.setSName("段昱如_1");
+            stu_1.setSTEL("000001");
+            stu_1.setCHI(122);
+            stu_1.setMATH(314);
+            stu_1.setENG(554);
+            student stu_2 = new student();
+            stu_2.setSID("B0226497_2");
+            stu_2.setSName("段昱如_2");
+            stu_2.setSTEL("000002");
+            stu_2.setCHI(-86);
+            stu_2.setMATH(-45);
+            stu_2.setENG(-45);
+            student stu_3 = new student();
+            stu_3.setSID("B0226497_3");
+            stu_3.setSName("段昱如_3");
+            stu_3.setSTEL("000003");
+            stu_3.setCHI(333);
+            stu_3.setMATH(-54);
+            stu_3.setENG(354);
+            student stu_4 = new student();
+            stu_4.setSID("B0226497_4");
+            stu_4.setSName("段昱如_4");
+            stu_4.setSTEL("000004");
+            stu_4.setCHI(433);
+            stu_4.setMATH(878);
+            stu_4.setENG(868);
+            student stu_5 = new student();
+            stu_5.setSID("B0226497_5");
+            stu_5.setSName("段昱如_5");
+            stu_5.setSTEL("000005");
+            stu_5.setCHI(-453);
+            stu_5.setMATH(-43);
+            stu_5.setENG(-43);
+            string str= "學生各項資料:";
+            str += $"\n學號:{stu_1.getSID()},姓名:{stu_1.getSName()},電話:{stu_1.getSTEL()},國文:{stu_1.getCHI()},數學:{stu_1.getMATH()},英文:{stu_1.getENG()},平均:{(stu_1.getCHI() + stu_1.getMATH() + stu_1.getENG()) / 3}";
+            str += $"\n學號:{stu_2.getSID()},姓名:{stu_2.getSName()},電話:{stu_2.getSTEL()},國文:{stu_2.getCHI()},數學:{stu_2.getMATH()},英文:{stu_2.getENG()},平均:{(stu_2.getCHI() + stu_2.getMATH() + stu_2.getENG()) / 3}";
+            str += $"\n學號:{stu_3.getSID()},姓名:{stu_3.getSName()},電話:{stu_3.getSTEL()},國文:{stu_3.getCHI()},數學:{stu_3.getMATH()},英文:{stu_3.getENG()},平均:{(stu_3.getCHI() + stu_3.getMATH() + stu_3.getENG()) / 3}";
+            str += $"\n學號:{stu_4.getSID()},姓名:{stu_4.getSName()},電話:{stu_4.getSTEL()},國文:{stu_4.getCHI()},數學:{stu_4.getMATH()},英文:{stu_4.getENG()},平均:{(stu_4.getCHI() + stu_4.getMATH() + stu_4.getENG()) / 3}";
+            str += $"\n學號:{stu_5.getSID()},姓名:{stu_5.getSName()},電話:{stu_5.getSTEL()},國文:{stu_5.getCHI()},數學:{stu_5.getMATH()},英文:{stu_5.getENG()},平均:{(stu_5.getCHI() + stu_5.getMATH() + stu_5.getENG()) / 3}";
+            lblresult.Text = str;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        class student
+        {
+            private string SID;
+            private string SName;
+            private string STEL;
+            private int CHI;
+            private int MATH;
+            private int ENG;
+            public void setSID(String sid)
+            {
+                SID = sid;
+            }
+            public String getSID()
+            {
+                return SID;
+            }
+            public void setSName(string sname)
+            {
+                SName = sname;
+            }
+            public string getSName()
+            {
+                return SName;
+            }
+            public void setSTEL(String stel)
+            {
+                STEL = stel;
+            }
+            public String getSTEL()
+            {
+                return STEL;
+            }
+            public void setCHI(int chi)
+            {
+                if (chi > 100)
+                    chi = 100;
+                else if (chi < 0)
+                    chi = 0;
+                CHI = chi;
+            }
+            public int getCHI()
+            {
+                return CHI;
+            }
+            public void setMATH(int math)
+            {
+                if (math > 100)
+                    math = 100;
+                else if (math < 0)
+                    math = 0;
+                MATH = math;
+            }
+            public int getMATH()
+            {
+                return MATH;
+            }
+            public void setENG(int eng)
+            {
+                if (eng > 100)
+                    eng = 100;
+                else if (eng < 0)
+                    eng = 0;
+                ENG = eng;
+            }
+            public int getENG()
+            {
+                return ENG;
+            }
+        }
+
+        private void lblresult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
